54835e623ac6e9b7129dd224c76baf51af2abf84
John Blackburn <jackblackburnn@icloud.com>
2025-08-19 20:38:26 -0400

    Add sanity checks and test errors

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,26 @@ impl Tensor {
     pub fn get_nd(&self, coords: &[usize]) -> Result<f32, String> {
         
         // TODO: check that coords are sensical
+        if self.shape.len() != coords.len() {
+            return Err( String::from("coords are nonsensical") );
+        }
 
         let mut idx = 0;
         let mut stride = 1;
 
-        for i in 0..coords.len() {
-            idx += stride * coords[i];
-            stride *= self.shape[i];
+        for dim in (0..coords.len()).rev() {
+            // iterate over coordinates from outer -> inner dimensions
+            // (row major)
+
+            let dimlen = self.shape[dim]; // current dimension size
+            let dimidx = coords[dim];     // coordinate idx along dimension
+            
+            if dimidx >= dimlen {
+                return Err( String::from("coords went out of bounds") );
+            }
+
+            idx += stride * dimidx; 
+            stride *= dimlen;      
         }
 
         Ok( self.elements[idx] )
@@ -37,4 +50,17 @@ mod tests {
         let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![1, 3]);
         assert_eq!( x.get_nd(&[0, 2]), Ok(-1.0) );
     }
+
+    #[test]
+    fn fail_from_coord_shape() {
+        let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![1, 3]);
+        assert_eq!( x.get_nd(&[0, 0, 0]), Err(String::from("coords are nonsensical")) );
+    }
+
+    #[test]
+    fn fail_get_element() {
+        let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![3, 1]);
+        assert_eq!( x.get_nd(&[0, 2]), Err(String::from("coords went out of bounds")) );
+    }
+
 }
