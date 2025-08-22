65b3de1e64698e912c5d277dfd415707490eafe2
John Blackburn <jackblackburnn@icloud.com>
2025-08-22 16:30:27 -0400

    Add custom tensor errors and delete goofy tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,87 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum TensorError {
+    RankMismatch {
+        provided: usize,
+        expected: usize,
+    },
+    CoordsOutOfBounds {
+        rank: usize,
+        provided: usize,
+        max: usize,
+    },
+}
+
+impl fmt::Display for TensorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TensorError::RankMismatch { provided, expected } => {
+                write!(
+                    f,
+                    "Coords do not Match Tensor rank: found: {}, expected: {}",
+                    provided, expected
+                )
+            }
+
+            TensorError::CoordsOutOfBounds {
+                rank,
+                provided,
+                max,
+            } => {
+                write!(
+                    f,
+                    "Coords went out of bounds at rank {}: found: {}, max: {}",
+                    rank, provided, max
+                )
+            }
+        }
+    }
+}
+
 pub struct Tensor {
     elements: Vec<f32>,
     shape: Vec<usize>,
 }
- 
-impl Tensor {
 
+impl Tensor {
     pub fn new(elements: Vec<f32>, shape: Vec<usize>) -> Self {
-        Tensor {
-            elements, 
-            shape,
-        }
+        Tensor { elements, shape }
     }
 
-    pub fn get_nd(&self, coords: &[usize]) -> Result<f32, String> {
-        
+    pub fn get_nd(&self, coords: &[usize]) -> Result<f32, TensorError> {
         // TODO: check that coords are sensical
         if self.shape.len() != coords.len() {
-            return Err( String::from("coords are nonsensical") );
+            return Err(TensorError::RankMismatch {
+                provided: coords.len(),
+                expected: self.shape.len(),
+            });
         }
 
         let mut idx = 0;
         let mut stride = 1;
 
-        for dim in (0..coords.len()).rev() {
-            // iterate over coordinates from outer -> inner dimensions
+        for rank in (0..coords.len()).rev() {
+            // iterate over coordinates from outer -> inner ranks
             // (row major)
 
-            let dimlen = self.shape[dim]; // current dimension size
-            let dimidx = coords[dim];     // coordinate idx along dimension
-            
-            if dimidx >= dimlen {
-                return Err( String::from("coords went out of bounds") );
+            let rank_len = self.shape[rank]; // current rank size
+            let rank_idx = coords[rank]; // coordinate idx along current rank
+
+            // check for out of bounds error
+            if rank_idx >= rank_len {
+                return Err(TensorError::CoordsOutOfBounds {
+                    rank: rank + 1,
+                    provided: rank_idx,
+                    max: rank_len - 1,
+                });
             }
 
-            idx += stride * dimidx; 
-            stride *= dimlen;      
+            idx += stride * rank_idx;
+            stride *= rank_len;
         }
 
-        Ok( self.elements[idx] )
+        Ok(self.elements[idx])
     }
 }
 
@@ -46,32 +90,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn get_element() {
-        let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![1, 3]);
-        assert_eq!( x.get_nd(&[0, 2]), Ok(-1.0) );
-    }
-
-    #[test]
-    fn fail_get_element() {
-        let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![1, 3]);
-        assert_eq!( x.get_nd(&[0, 3]), Err(String::from("coords went out of bounds")) );
-    }
-
-    #[test]
-    fn fail_get_element2() {
-        let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![3, 1]);
-        assert_eq!( x.get_nd(&[0, 2]), Err(String::from("coords went out of bounds")) );
-    }
-
-    #[test]
-    fn fail_from_coord_shape() {
-        let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![1, 3]);
-        assert_eq!( x.get_nd(&[0, 0, 0]), Err(String::from("coords are nonsensical")) );
-    }
-
-    #[test]
-    fn fail_from_coord_shape1() {
-        let x = Tensor::new( vec![1.0, 0.0, -1.0], vec![1, 3]);
-        assert_eq!( x.get_nd(&[0]), Err(String::from("coords are nonsensical")) );
+    fn get_vector_element() {
+        let x = Tensor::new(vec![1.0, 0.0, -1.0], vec![3]);
+        assert_eq!(x.get_nd(&[0]).unwrap(), 1.0);
     }
 }
