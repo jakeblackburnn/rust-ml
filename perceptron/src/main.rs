f9120dc92cc8b23d560af26c0deb270376c44896
John Blackburn <jackblackburnn@icloud.com>
2025-10-27 19:26:56 -0400

    Add practical test to main

--- perceptron/src/main.rs
+++ perceptron/src/main.rs
@@ -2,13 +2,22 @@ use perceptron::Perceptron;
 use std::process;
 
 fn main() {
-    let mut model = Perceptron::new(3);
+    let mut model = Perceptron::new(3); 
 
+    println!("training model...");
     model.fit("train.dat").unwrap_or_else(|err| {
         eprintln!("Error during training: {}", err);
         process::exit(1);
     });
 
+
+    println!("evaluating model on training data...");
+    model.evaluate("train.dat").unwrap_or_else(|err| {
+        eprintln!("Error during evaluation: {}", err);
+        process::exit(1);
+    });
+
+    println!("evaluating model on testing data...");
     model.evaluate("test.dat").unwrap_or_else(|err| {
         eprintln!("Error during evaluation: {}", err);
         process::exit(1);
